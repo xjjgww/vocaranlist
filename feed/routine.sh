97b5f262dba8ec08f79a871c186c10f5d0cb9ec8
xjjgww <xjjgww@gmail.com>
2022-12-05 17:36:17 +0100

    fix conflict

--- feed/routine.sh
+++ feed/routine.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 makejson.py
+python3.9 makejson.py
 echo
-python3 listuta.py # slow
+python3.9 listuta.py # slow
 echo
-python3 fixlistuta.py
+python3.9 fixlistuta.py
 echo
-python3 songdb.py # slow
+python3.9 songdb.py # slow
 echo
 
