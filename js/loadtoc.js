fe8e2bd2963aefbf384e295f62350d657db8caee
xjjgww <xjjgww@gmail.com>
2023-05-20 17:16:30 +0200

    add #815

--- js/loadtoc.js
+++ js/loadtoc.js
@@ -3,75 +3,75 @@ var nicopl = ['3165526', '3271200', '3409323', '2951756', '3900779']; // playlis
 
 function loadtoc()
 {
-    var thetoc = document.getElementById('toc');
-    var count = 0;
-    for(var sm in episodejsonobj)
-    {
-        var theitem = episodejsonobj[sm];
-        var mylink = 'https://www.nicovideo.jp/watch/' + sm;
-        var songrium = 'http://songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp%252Frss%252F' + sm;
-        var episode = theitem[0];
-        var number = theitem[1];
-        var datetxt = theitem[2];
-        var lineid = '#'+episode+' '+sm+' '+datetxt;
+  var thetoc = document.getElementById('toc');
+  var count = 0;
+  for(var sm in episodejsonobj)
+  {
+    var theitem = episodejsonobj[sm];
+    var mylink = 'https://www.nicovideo.jp/watch/' + sm;
+    var songrium = 'http://songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp%252Frss%252F' + sm;
+    var episode = theitem[0];
+    var number = theitem[1];
+    var datetxt = theitem[2];
+    var lineid = '#'+episode+' '+sm+' '+datetxt;
 
-        var line = document.createElement("div");
-        line.setAttribute('type', 'line');
-        line.id = lineid;
-        thetoc.appendChild(line);
+    var line = document.createElement("div");
+    line.setAttribute('type', 'line');
+    line.id = lineid;
+    thetoc.appendChild(line);
 
-        var btnn = document.createElement('button');
-        btnn.id = 'n'+sm;
-        btnn.className = "btn left linkp";
-        btnn.innerHTML = number;
-        line.appendChild(btnn);
-        btnn.setAttribute("onclick", "window.open('https://www.nicovideo.jp/watch/"+sm+"','_blank')");
-        btnn.setAttribute("onmouseover", "statusbar('https://www.nicovideo.jp/watch/"+sm+"')");
-        btnn.setAttribute("onmouseout", "statusbar('&nbsp;')");
+    var btnn = document.createElement('button');
+    btnn.id = 'n'+sm;
+    btnn.className = "btn left linkp";
+    btnn.innerHTML = number;
+    line.appendChild(btnn);
+    btnn.setAttribute("onclick", "window.open('https://www.nicovideo.jp/watch/"+sm+"','_blank')");
+    btnn.setAttribute("onmouseover", "statusbar('https://www.nicovideo.jp/watch/"+sm+"')");
+    btnn.setAttribute("onmouseout", "statusbar('&nbsp;')");
 
-        var btnvn = document.createElement('button');
-        btnvn.className = "btn left hovercolor hideinmobile";
-        btnvn.innerHTML = '<i class="fa-regular fa-clone"></i>';
-        line.appendChild(btnvn);
-        btnvn.setAttribute("onclick", "copylink('"+sm+"', 'the sm #')");
-        btnvn.setAttribute("onmouseover", "statusbar('https://www.nicovideo.jp/watch/"+sm+"')");
-        btnvn.setAttribute("onmouseout", "statusbar('&nbsp;')");
+    var btnvn = document.createElement('button');
+    btnvn.className = "btn left hovercolor hideinmobile";
+    btnvn.innerHTML = '<i class="fa-regular fa-clone"></i>';
+    line.appendChild(btnvn);
+    btnvn.setAttribute("onclick", "copylink('"+sm+"', 'the sm #')");
+    btnvn.setAttribute("onmouseover", "statusbar('https://www.nicovideo.jp/watch/"+sm+"')");
+    btnvn.setAttribute("onmouseout", "statusbar('&nbsp;')");
 
-        var btnb = document.createElement('button');
-        btnb.id = 'b'+sm;
-        btnb.className = "btn left linkp hideinmobile";
-        btnb.innerHTML = sm;
-        line.appendChild(btnb);
-        btnb.setAttribute("onclick", "window.open('http://songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp%252Frss%252F"+sm+"', '_blank')");
-        btnb.setAttribute("onmouseover", "statusbar('songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp//rss//"+sm+"')");
-        btnb.setAttribute("onmouseout", "statusbar('&nbsp;')");
+    var btnb = document.createElement('button');
+    btnb.id = 'b'+sm;
+    btnb.className = "btn left linkp hideinmobile";
+    btnb.innerHTML = sm;
+    line.appendChild(btnb);
+    btnb.setAttribute("onclick", "window.open('http://songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp%252Frss%252F"+sm+"', '_blank')");
+    btnb.setAttribute("onmouseover", "statusbar('songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp//rss//"+sm+"')");
+    btnb.setAttribute("onmouseout", "statusbar('&nbsp;')");
 
-        var btnvs = document.createElement('button');
-        btnvs.className = "btn left hovercolor hideinmobile";
-        btnvs.innerHTML = '<i class="fa-regular fa-clone"></i>';
-        line.appendChild(btnvs);
-        btnvs.setAttribute("onclick", "copylink('http://songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp%252Frss%252F"+sm+"', 'the songrium link')");
-        btnvs.setAttribute("onmouseover", "statusbar('songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp//rss//"+sm+"')");
-        btnvs.setAttribute("onmouseout", "statusbar('&nbsp;')");
+    var btnvs = document.createElement('button');
+    btnvs.className = "btn left hovercolor hideinmobile";
+    btnvs.innerHTML = '<i class="fa-regular fa-clone"></i>';
+    line.appendChild(btnvs);
+    btnvs.setAttribute("onclick", "copylink('http://songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp%252Frss%252F"+sm+"', 'the songrium link')");
+    btnvs.setAttribute("onmouseover", "statusbar('songrium.jp/map/#!/playlist?type=feed&feed_uri=nicodb.jp//rss//"+sm+"')");
+    btnvs.setAttribute("onmouseout", "statusbar('&nbsp;')");
 
-        var btnd = document.createElement('button');
-        btnd.id = 'd'+sm;
-        btnd.className = "btn left";
-        btnd.innerHTML = datetxt;
-        line.appendChild(btnd);
-        btnd.setAttribute("onclick", "darkendate('"+sm+"'); document.getElementById('rightpanel').style.display = 'block';");
+    var btnd = document.createElement('button');
+    btnd.id = 'd'+sm;
+    btnd.className = "btn left";
+    btnd.innerHTML = datetxt;
+    line.appendChild(btnd);
+    btnd.setAttribute("onclick", "darkendate('"+sm+"'); document.getElementById('rightpanel').style.display = 'block';");
 
-        var btnpl = document.createElement('button');
-        btnpl.className = "btn right linkp";
-        btnpl.innerHTML = "PL";
-        line.appendChild(btnpl);
-        btnpl.setAttribute("onclick", "window.open('https://www.nicovideo.jp/mylist/"+nicopl[episode%5]+"', '_blank')");
-        btnpl.setAttribute("onmouseover", "statusbar('&#187; Playlist &#187;.')");
-        btnpl.setAttribute("onmouseout", "statusbar('&nbsp;')");
-        if(count >= 5) { btnpl.style.display = "none"; }
+    var btnpl = document.createElement('button');
+    btnpl.className = "btn right linkp";
+    btnpl.innerHTML = "PL";
+    line.appendChild(btnpl);
+    btnpl.setAttribute("onclick", "window.open('https://www.nicovideo.jp/mylist/"+nicopl[episode%5]+"', '_blank')");
+    btnpl.setAttribute("onmouseover", "statusbar('&#187; Playlist &#187;.')");
+    btnpl.setAttribute("onmouseout", "statusbar('&nbsp;')");
+    if(count >= 5) { btnpl.style.display = "none"; }
 
-        if(count==0) darkendate(sm);
-        count = count+1;
-    }
-    altercolor();
+    if(count==0) darkendate(sm);
+    count = count+1;
+  }
+  altercolor();
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,137 +1,137 @@
 
 function myFunction() {
-    document.getElementById("title").innerHTML = "Welcome!";
+  document.getElementById("title").innerHTML = "Welcome!";
 }
 
 function altercolor() {
-    var lines = document.getElementsByTagName('div');
-    var filteri = 0;
-    for(var i=0; i<lines.length; i++)
-    {
-        if(lines[i].getAttribute('type') != "line") continue;
-        if(lines[i].style.display == "none") continue;
-        if(filteri%2==0) { lines[i].className = "btn-group gray-background"; }
-        else { lines[i].className = "btn-group white-background"; }
-        filteri++;
-    }    
+  var lines = document.getElementsByTagName('div');
+  var filteri = 0;
+  for(var i=0; i<lines.length; i++)
+  {
+    if(lines[i].getAttribute('type') != "line") continue;
+    if(lines[i].style.display == "none") continue;
+    if(filteri%2==0) { lines[i].className = "btn-group gray-background"; }
+    else { lines[i].className = "btn-group white-background"; }
+    filteri++;
+  }    
 }
 
 function myfilter() {
-    var input, i, lines, idd;
-    input = document.getElementById('filterinput').value;
-    lines = document.getElementsByTagName('div');
-    for(i=0; i<lines.length; i++)
+  var input, i, lines, idd;
+  input = document.getElementById('filterinput').value;
+  lines = document.getElementsByTagName('div');
+  for(i=0; i<lines.length; i++)
+  {
+    if(lines[i].getAttribute('type') != "line") continue;
+    idd = lines[i].id;
+    var sm = idd.match(/sm\d+/g);
+    var findmatch = '';
+    for(var jitem in songjsonobj[sm])
     {
-        if(lines[i].getAttribute('type') != "line") continue;
-        idd = lines[i].id;
-        var sm = idd.match(/sm\d+/g);
-        var findmatch = '';
-        for(var jitem in songjsonobj[sm])
-        {
-            var songsm = songjsonobj[sm][jitem]["id"];
-            findmatch += (' '+songjsonobj[sm][jitem]["alt"]);
-            if(songsm in songdbjsonobj) { findmatch += (' '+songdbjsonobj[songsm]["owner"]+' '+songdbjsonobj[songsm]['postedDateTime']); }
-        }
-        if(idd.indexOf(input) > -1 || findmatch.indexOf(input) > -1) { lines[i].style.display = ""; }
-        else { lines[i].style.display = "none"; }
+      var songsm = songjsonobj[sm][jitem]["id"];
+      findmatch += (' '+songjsonobj[sm][jitem]["alt"]);
+      if(songsm in songdbjsonobj) { findmatch += (' '+songdbjsonobj[songsm]["owner"]+' '+songdbjsonobj[songsm]['postedDateTime']); }
     }
-    altercolor();
-    searchmatch();
+    if(idd.indexOf(input) > -1 || findmatch.indexOf(input) > -1) { lines[i].style.display = ""; }
+    else { lines[i].style.display = "none"; }
+  }
+  altercolor();
+  searchmatch();
 }
 
 function copylink(str, noti) {
-    const el = document.createElement('textarea');
-    el.value = str;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    document.getElementById('clipboard').innerHTML = "Copied "+noti+".";
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  document.getElementById('clipboard').innerHTML = "Copied "+noti+".";
 }
 
 function darkendate(sm) {
-    var thetab = document.getElementById('songlist');
-    for(var i=0; i<100; i++)
+  var thetab = document.getElementById('songlist');
+  for(var i=0; i<100; i++)
+  {
+    var eletr = document.getElementById('tr'+i);
+    if(eletr !== null) { eletr.remove(); }
+  }
+  var input = document.getElementById('filterinput').value;
+  document.getElementById('tr-1').innerHTML = episodejsonobj[sm][1]+' '+episodejsonobj[sm][2];
+  for(var i=0; i<songjsonobj[sm].length; i++)
+  {
+    var songsm = songjsonobj[sm][i]['id'];
+    var newtr = document.createElement("tr");
+    newtr.id = 'tr'+i;
+    newtr.className = "songitem";
+    thetab.appendChild(newtr);
+    var newtp = document.createElement("td");
+    newtp.id = 'tp'+i;
+    newtp.className = "thumbnail";
+    newtr.appendChild(newtp);
+    var newpa = document.createElement("a");
+    newpa.href = "https://www.nicovideo.jp/watch/"+songsm;
+    newpa.setAttribute('target', '_blank');
+    newtp.appendChild(newpa);
+    var newpp = document.createElement("img");
+    if(songsm in songdbjsonobj)
     {
-        var eletr = document.getElementById('tr'+i);
-        if(eletr !== null) { eletr.remove(); }
+      newpp.src = songdbjsonobj[songsm]["largeThumbnailURL"];
+      // newpp.src = songdbjsonobj[songsm]["thumbnailURL"];
     }
-    var input = document.getElementById('filterinput').value;
-    document.getElementById('tr-1').innerHTML = episodejsonobj[sm][1]+' '+episodejsonobj[sm][2];
-    for(var i=0; i<songjsonobj[sm].length; i++)
-    {
-        var songsm = songjsonobj[sm][i]['id'];
-        var newtr = document.createElement("tr");
-        newtr.id = 'tr'+i;
-        newtr.className = "songitem";
-        thetab.appendChild(newtr);
-        var newtp = document.createElement("td");
-        newtp.id = 'tp'+i;
-        newtp.className = "thumbnail";
-        newtr.appendChild(newtp);
-        var newpa = document.createElement("a");
-        newpa.href = "https://www.nicovideo.jp/watch/"+songsm;
-        newpa.setAttribute('target', '_blank');
-        newtp.appendChild(newpa);
-        var newpp = document.createElement("img");
-        if(songsm in songdbjsonobj)
-        {
-            newpp.src = songdbjsonobj[songsm]["largeThumbnailURL"];
-            // newpp.src = songdbjsonobj[songsm]["thumbnailURL"];
-        }
-        else { newpp.src = 'img/deletethumb.png'; }
-        newpa.appendChild(newpp);
-        var newtd = document.createElement("td");
-        newtd.id = 'td'+i;
-        newtd.style.padding = "1vw";
-        newtr.appendChild(newtd);
-        var newtda = document.createElement("a");
-        newtda.id = 'tda'+i;
-        newtda.className = "songtext";
-        var songname = '<p class="info">'+songjsonobj[sm][i]['title']+'</p></br><i class="fa-solid fa-user"></i> ';
-        if(songsm in songdbjsonobj)
-            songname += '<p class="info">'+songdbjsonobj[songsm]['owner']+'</p></br>';
-        songname += '<i class="fa-solid fa-upload"></i> ';
-        if(songsm in songdbjsonobj)
-            songname += '<p class="info">'+songdbjsonobj[songsm]['postedDateTime']+'</p></br>';
-        newtda.innerHTML = songname;
-        newtda.href = "https://www.nicovideo.jp/watch/"+songsm;
-        newtda.setAttribute('target', '_blank');
-        newtd.appendChild(newtda);
-    }
-    searchmatch();        
+    else { newpp.src = 'img/deletethumb.png'; }
+    newpa.appendChild(newpp);
+    var newtd = document.createElement("td");
+    newtd.id = 'td'+i;
+    newtd.style.padding = "1vw";
+    newtr.appendChild(newtd);
+    var newtda = document.createElement("a");
+    newtda.id = 'tda'+i;
+    newtda.className = "songtext";
+    var songname = '<p class="info">'+songjsonobj[sm][i]['title']+'</p></br><i class="fa-solid fa-user"></i> ';
+    if(songsm in songdbjsonobj)
+      songname += '<p class="info">'+songdbjsonobj[songsm]['owner']+'</p></br>';
+    songname += '<i class="fa-solid fa-upload"></i> ';
+    if(songsm in songdbjsonobj)
+      songname += '<p class="info">'+songdbjsonobj[songsm]['postedDateTime']+'</p></br>';
+    newtda.innerHTML = songname;
+    newtda.href = "https://www.nicovideo.jp/watch/"+songsm;
+    newtda.setAttribute('target', '_blank');
+    newtd.appendChild(newtda);
+  }
+  searchmatch();        
 }
 function fadedate(sm) {
-    document.getElementById('d'+sm).style.opacity = "0.8";
+  document.getElementById('d'+sm).style.opacity = "0.8";
 }
 
 function statusbar(str) {
-    document.getElementById('clipboard').innerHTML = str;
+  document.getElementById('clipboard').innerHTML = str;
 }
 
 function appinput(str) {
-    document.getElementById('filterinput').value += str;
-    myfilter();
+  document.getElementById('filterinput').value += str;
+  myfilter();
 }
 
 function goback() {
-    document.getElementById('ranlist').style.display = "none";
-    document.getElementById('back').style.display = "none";
-    document.getElementById('filtergroup_m').style.display = "block";
+  document.getElementById('ranlist').style.display = "none";
+  document.getElementById('back').style.display = "none";
+  document.getElementById('filtergroup_m').style.display = "block";
 }
 
 function searchmatch() {
-    var input = document.getElementById('filterinput').value;
-    var songname = document.getElementsByClassName('info');
-    var matchstyle = 'class="match"';
-    for(let i = 0;i < songname.length; i++)
-    {
-        var ss = songname[i];
-        var songtext = ss.innerHTML;
-        songtext = songtext.replaceAll('<a '+matchstyle+'>', '');
-        songtext = songtext.replaceAll('</a>', '');
-        if(input != '')
-            songtext = songtext.replaceAll(input, '<a '+matchstyle+'>' + input + '</a>');
-        ss.innerHTML = songtext;
-    }
+  var input = document.getElementById('filterinput').value;
+  var songname = document.getElementsByClassName('info');
+  var matchstyle = 'class="match"';
+  for(let i = 0;i < songname.length; i++)
+  {
+    var ss = songname[i];
+    var songtext = ss.innerHTML;
+    songtext = songtext.replaceAll('<a '+matchstyle+'>', '');
+    songtext = songtext.replaceAll('</a>', '');
+    if(input != '')
+      songtext = songtext.replaceAll(input, '<a '+matchstyle+'>' + input + '</a>');
+    ss.innerHTML = songtext;
+  }
 }
